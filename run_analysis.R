40ef24b5dea24b7d8190178d8083bba71e795283
BoSab <bosabljic@gmail.com>
2016-05-08 14:38:32 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 library(dplyr)
-# part 1
+## 1. Merges the training and the test sets to create one data set
 features<-as.character(read.table('UCI HAR Dataset/features.txt')[,2])
 xtest<-read.table('UCI HAR Dataset/test/X_test.txt',col.names=features)
 subjtest<-read.table('UCI HAR Dataset/test/subject_test.txt',col.names='subject')
@@ -12,22 +12,22 @@ data_train<-cbind(xtrain,subjtrain,ytrain)
 data_test<-cbind(xtest,subjtest,ytest)
 combdata<-rbind(data_train,data_test)
 
-# part 2
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 mean_std<-grepl('.mean',names(combdata))&!grepl('.meanFreq',names(combdata))|grepl('.std',names(combdata))
 sel_col<-c(1:length(names(combdata)))[mean_std]
 data<-select(combdata,sel_col,subject,activity)
 
-# part 3
+## 3. Uses descriptive activity names to name the activities in the data set
 data<-mutate(data,activity=actlab[match(activity,actlab$activity_code),2])
 
-#part 4
+## 4. Appropriately labels the data set with descriptive activity names.
 write.table(names(data),'names.txt',row.name=FALSE)
 # at this point changes were made separately in the .txt file which was renamed "names1.txt" upon saving
 names<-read.table('names1.txt')
 names<-names[2:69,1]
 colnames(data) <- make.names(names)
 
-#part 5
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 group<-group_by(data,subject,activity)
 data2<-summarise_each(group,funs(mean),1:68)
 write.table(data2,'dataset.txt',row.name=FALSE)  
